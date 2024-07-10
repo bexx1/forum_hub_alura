a78015f72277a2edbdbcbea0793ca1345f9586f0
bexx1 <rebecasfigueiro@gmail.com>
2024-07-10 19:27:45 -0300

    Creating user JPA and it`s authentication and repository folders. Adding a migration to create a table users in the database.

---
+++ src/main/java/com/example/alura/forum/domain/user/User.java
@@ -0,0 +1,21 @@
+package com.example.alura.forum.domain.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "users")
+@Entity(name = "user")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    private String username;
+    private String password;
+}
