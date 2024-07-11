2942d32713418a778b3aa7e150b671edcae81b3b
bexx1 <rebecasfigueiro@gmail.com>
2024-07-11 16:03:25 -0300

    Adding the Token JWT to the code

--- src/main/java/com/example/alura/forum/controller/AuthenticationController.java
+++ src/main/java/com/example/alura/forum/controller/AuthenticationController.java
@@ -1,6 +1,9 @@
 package com.example.alura.forum.controller;
 
+import com.example.alura.forum.domain.user.User;
 import com.example.alura.forum.domain.user.dto.AuthenticationDTO;
+import com.example.alura.forum.infra.security.TokenJWTDTO;
+import com.example.alura.forum.infra.security.TokenService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -16,13 +19,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthenticationController {
     @Autowired
     private AuthenticationManager manager;
+    @Autowired
+    private TokenService tokenService;
 
     @PostMapping
     public ResponseEntity signIn(@RequestBody @Valid AuthenticationDTO dto) {
-        var token = new UsernamePasswordAuthenticationToken(dto.username(), dto.password());
-        var authentication = manager.authenticate(token);
+        var authenticationToken = new UsernamePasswordAuthenticationToken(dto.username(), dto.password());
+        var authentication = manager.authenticate(authenticationToken);
 
-        return ResponseEntity.ok().build();
+        var tokenJWT = tokenService.generateToken((User) authentication.getPrincipal());
+
+        return ResponseEntity.ok(new TokenJWTDTO(tokenJWT));
     }
 }
 
