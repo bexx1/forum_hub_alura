89416f3d5ce0c7fea2f34448c908400b9c1f3adc
bexx1 <rebecasfigueiro@gmail.com>
2024-07-11 16:49:04 -0300

    Validating the token and getting the user that is making the request

--- src/main/java/com/example/alura/forum/infra/security/SecurityFilter.java
+++ src/main/java/com/example/alura/forum/infra/security/SecurityFilter.java
@@ -1,9 +1,11 @@
 package com.example.alura.forum.infra.security;
 
+import com.example.alura.forum.infra.security.token.TokenService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -12,11 +14,25 @@ import java.io.IOException;
 // A filter is called before the controllers, so you can intercept the api requests and execute something or block certain requests
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
+    @Autowired
+    TokenService tokenService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var tokenJWT = getToken(request);
+        var user = tokenService.getSubject(tokenJWT);
+
         filterChain.doFilter(request, response); // this code is necessary to call the next filters in the application
     }
 
+    private String getToken(HttpServletRequest request) {
+        // the token must be sent in a header authorization by the user that is making an api request
+        var authorizationHeader = request.getHeader("Authorization");
 
+        if (authorizationHeader == null) {
+            throw new RuntimeException("The jwt token was not sent in the Authorization header");
+        }
+
+        return authorizationHeader.replace("Bearer ", "");
+    }
 }
