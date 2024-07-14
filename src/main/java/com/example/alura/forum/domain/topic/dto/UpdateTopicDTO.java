6e337934cba31a2e517fa707f1d41be4b7622f31
bexx1 <rebecasfigueiro@gmail.com>
2024-07-14 18:55:13 -0300

    Making the update dto have it`s variables optionals.

--- src/main/java/com/example/alura/forum/domain/topic/dto/UpdateTopicDTO.java
+++ src/main/java/com/example/alura/forum/domain/topic/dto/UpdateTopicDTO.java
@@ -8,11 +8,8 @@ import jakarta.validation.constraints.NotNull;
 public record UpdateTopicDTO(
         @NotNull
         Long id,
-        @NotBlank(message = "The title is mandatory")
         String title,
-        @NotBlank(message = "The message is mandatory")
         String message,
-        @NotBlank(message = "The course is mandatory")
         String course
         ) {
 }
